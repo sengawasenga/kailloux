becadd8d751a973d50cbdebf7774b5fc75b316e6
sengawasenga <14marcel.senga@gmail.com>
2026-02-24 00:17:23 +0100

    feat: add Hero component to display a prominent introductory section on the homepage.

--- components/site/home/hero.tsx
+++ components/site/home/hero.tsx
@@ -5,7 +5,7 @@ const Hero = () => {
   return (
     <section
       id="heroSection"
-      className="min-h-screen flex pt-[150px] md:pt-[200px]"
+      className="min-h-screen flex flex-col pt-[150px] md:pt-[200px] gap-10"
     >
       <div className="px-5 md:px-8 max-w-7xl mx-auto">
         <div className="grid grid-cols-1 md:grid-cols-2 gap-12 lg:gap-24">
@@ -25,6 +25,13 @@ const Hero = () => {
             </Button>
           </div>
         </div>
+      </div>
+
+      <div className="mx-auto max-w-[1500px] w-full overflow-hidden flex justify-center">
+        <div className="rounded-full h-[450px] md:h-[600px] lg:h-[700px] min-w-[280px] sm:min-w-[350px] w-full bg-primary overflow-hidden relative"></div>
+        <div className="rounded-full h-[450px] md:h-[600px] lg:h-[700px] min-w-[280px] sm:min-w-[350px] w-full bg-primary-dark overflow-hidden relative mt-[150px]"></div>
+        <div className="rounded-full h-[450px] md:h-[600px] lg:h-[700px] min-w-[280px] sm:min-w-[350px] w-full bg-secondary overflow-hidden relative mt-[50px]"></div>
+        <div className="rounded-full h-[450px] md:h-[600px] lg:h-[700px] min-w-[280px] sm:min-w-[350px] w-full bg-primary overflow-hidden relative mt-[100px]"></div>
       </div>
     </section>
   );
